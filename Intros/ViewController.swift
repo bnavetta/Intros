e9b0f92ddd3a4c146bfb8c64a058853cce07b491
Ben Navetta <ben.navetta@gmail.com>
2016-01-07 23:45:13 -0500

    initial test generating QR code

--- Intros/ViewController.swift
+++ Intros/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet
+    weak var imageView: UIImageView!
+    
+    let user: BNIUser = {
+        let myUser = BNIUser();
+        myUser.firstName = "Ben";
+        myUser.lastName = "Navetta";
+        myUser.phoneNumber = "9783023343";
+        myUser.facebook = "100001646049061";
+        myUser.snapchat = "ben.navetta";
+        myUser.twitter = "BenNavetta";
+        return myUser;
+    }();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let qrCode = createQRCode(user) {
+            imageView.image = createNonInterpolatedUIImageFromCIImage(qrCode, scale: 2 * UIScreen.mainScreen().scale);
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // https://github.com/ShinobiControls/iOS7-day-by-day/blob/master/15-core-image-filters/15-core-image-filters.md
 
-
+    func createQRCode(user: BNIUser) -> CIImage? {
+        guard let data = user.data() else {
+            return nil;
+        }
+        
+        guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
+            return nil;
+        }
+        
+        filter.setValue(data, forKey: "inputMessage");
+        filter.setValue("H", forKey: "inputCorrectionLevel");
+        
+        return filter.outputImage;
+    }
+    
+    func createNonInterpolatedUIImageFromCIImage(image: CIImage, scale: CGFloat) -> UIImage {
+        let cgImage = CIContext(options: nil).createCGImage(image, fromRect: image.extent);
+        
+        UIGraphicsBeginImageContext(CGSize(width: image.extent.size.width * scale, height: image.extent.size.height * scale));
+        let context = UIGraphicsGetCurrentContext();
+        
+        defer {
+            UIGraphicsEndImageContext();
+        }
+        
+        // The image is pixel-correct, so don't interpolate
+        CGContextSetInterpolationQuality(context, CGInterpolationQuality.None);
+        CGContextDrawImage(context, CGContextGetClipBoundingBox(context), cgImage);
+        
+        return UIGraphicsGetImageFromCurrentImageContext();
+    }
 }
 
